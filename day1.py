2d40d0b037138f47299f4c79a964b1255b928a36
Evan Johnson <johnsonevanpatrick@gmail.com>
2020-12-06 13:27:30 -0700

    day 1 completed

---
+++ day1.py
@@ -0,0 +1,16 @@
+
+f = open("input.txt", "r")
+
+arr = []
+for x in f:
+    y = int(x)
+    arr.append(y)
+
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        for k in range(len(arr)):
+            if arr[i] + arr[j] + arr[k] == 2020:
+                print(arr[i] * arr[j] * arr[k])
+
+    
